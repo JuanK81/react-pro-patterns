5daaf862e87b9493b63902957240ec98248bd958
JuanK81 <jcasalsbadolato@gmail.com>
2022-07-13 16:38:56 +0200

    basic component pattern

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,21 +1,29 @@
 import { useProduct } from '../hooks/useProduct';
 
 import styles from '../styles/styles.module.css';
-// import noImage from '../assets/no-image.jpg';
+import noImage from '../assets/no-image.jpg';
 
-export const ProductCard = (): JSX.Element => {
+
+interface Props {
+    product: Product;
+}
+
+interface Product {
+    id: string;
+    title: string;
+    img?: string;
+}
+
+
+export const ProductCard = ({ product }: Props): JSX.Element => {
     const { counter, increaseBy } = useProduct();
 
   return (
     <div className={styles.productCard}>
-      <img
-        className={styles.productImg}
-        src="./coffee-mug.png"
-        alt="coffee mug"
-      />
-      {/* <img className={styles.productImg} src={noImage} alt="no img" /> */}
+      
+      <img className={styles.productImg} src={product.img ? product.img : noImage} alt="no img" />
 
-      <span className={styles.productDescription}>Coffee Mug</span>
+      <span className={styles.productDescription}>{ product.title }</span>
 
       <div className={styles.buttonsContainer}>
         <button className={styles.buttonMinus} onClick={() => increaseBy(-1)}>
--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,11 +1,23 @@
 import { ProductCard } from '../components/ProductCard';
 
-export const ShoppingPage = (): JSX.Element  => {
+const product = {
+  id: '1',
+  title: 'Coffee Mug - Card',
+  img: './coffee-mug.png'
+}
+
+export const ShoppingPage = (): JSX.Element => {
   return (
     <div>
       <h1>Shopping Store</h1>
       <hr />
-      <ProductCard />
+      <div style={{
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap:  'wrap'
+      }}>
+        <ProductCard product={ product }/>
+      </div>
     </div>
   );
 };
